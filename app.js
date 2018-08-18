75a0dc7f8ae576f00dd1fec045d245c1fb2a2098
vicky-ding <lijuan_ding@163.com>
2018-08-18 10:32:14 +0800

    编写自定义router

--- app.js
+++ app.js
@@ -7,14 +7,15 @@ const bodyParser = require('koa-bodyparser')
 
 const app = new Koa()
 
-app.use(serve(__dirname + '/web/dist'))
-app.use(bodyParser({
+app.use(serve(__dirname + '/web/dist'))  //app.use中间件
+app.use(bodyParser({        //bodyParser插件  将前端参数格式化
   formLimit: '10mb',
   jsonLimit: '10mb'
 }))
 
+//ctx 请求的上下文，前端请求的对象，可以取前端的数据
 app.use(async (ctx, next) => {
-  var requestId = uuid.v4()
+  var requestId = uuid.v4()  //uuid生成唯一的字符串, v4随机生成，v1按照时间戳生成
   ctx.err = function (err) {
     ctx.body = {
       message: err,
@@ -22,17 +23,49 @@ app.use(async (ctx, next) => {
     }
   }
 
-  ctx.set('X-Request-Id', requestId)
-  ctx.set('Access-Control-Allow-Origin', '*')
-  await next()
+  ctx.set('X-Request-Id', requestId)   // 设置响应头
+  ctx.set('Access-Control-Allow-Origin', '*')  //Access-Control-Allow-Origin 允许跨域
+  await next()   //await 同步
 })
 
+// koa-router内部实现原理
+/*app.use(async (ctx, next) => {
+  path = ctx.host
+  switch(path) {
+    case '/user':
+    let users = await UserModel.findByName('admin');
+    ctx.body = {
+      message: users,
+      stat: STAT.STAT_OK
+    }
+  }
+})*/
+
+// context
+// 自定义router
+app.use(async (ctx, next) => {
+  switch (ctx.path) {
+    case '/dinglijuan':
+      return ctx.body = {
+        stat: 'OK',
+        message: 'hello dinglijuan'
+      }
+      break;
+  
+    default:
+      await next()
+      break;
+  }
+})
+
+// 创建接口
 router.all('/user', require('./route/user'))
 router.use('/otherapp', require('./route/otherapp').routes())
 
 const routes = router.routes()
 app.use(routes)
 
+// 返回404错误
 app.use(async (ctx, next) => {
   if (ctx.errors) {
     var keys = Object.keys(ctx.errors[0])
@@ -44,7 +77,7 @@ app.use(async (ctx, next) => {
     }
   }
 })
-
+// 监听所有的错误
 app.on('error', async (err, ctx) => {
   console.error(err)
   ctx.err(err.message)
--- route/otherapp.js
+++ route/otherapp.js
@@ -2,10 +2,12 @@ const STAT = require('../lib/stat')
 const router = require('koa-router')()
 
 router.all('/', async (ctx, next) => {
-  ctx.body = {
+  /*ctx.body = {
     message: [],
     stat: STAT.STAT_OK
-  }
+  }*/
+  console.log('===')
+  await next()
 })
 
 router.all('/all', async (ctx, next) => {
